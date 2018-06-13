336b0740cb308d0ca850ab6230351c7b2b5ff4cf
DanilRostov <daniil.rossy@gmail.com>
2018-06-13 17:17:07 +0300

    fixed splitAndMerge function

--- splitAndMerge.js
+++ splitAndMerge.js
@@ -1,47 +1,16 @@
-// Первая реализация выполнена в соответсвии с алгоритмом, прописанным в задании
 const splitAndMerge = ( str, sp ) => {
-	let words = str.split( ' ' );
-	let chars = '';
+	const words = str.split( ' ' );
 	let result = '';
 
 	for( let i = 0; i < words.length; i += 1 ) {
-		if( !words[i] ) {
-			continue;
+		if( words[i + 1] ) {
+			result += words[i].split('').join(sp) + ' ';
 		} else {
-			chars += words[i].split(' ');
-		}
-	}
-
-	for( let i = 0; i < chars.length; i += 1 ) {
-		if( !chars[i + 1] ) {
-			result += chars[i];
-		} else {
-			result += chars[i] + sp;
+			result += words[i].split('').join(sp);
 		}
 	}
 
 	return result;
 }
 
-console.log( splitAndMerge("Hello World!", ':') );
-
-
-// Вторая реализация выполнена по собственному алгоритму
-const splitAndMerge2 = ( str, sp ) => {
-	let result = '';
-
-	for( let i = 0; i < str.length; i += 1 ) {
-		if( str[i] === ' ' ) {
-			continue;
-		} else if( str[i + 1] === undefined ) {
-			result += str[i];
-		} else {
-			result += str[i] + sp;
-		}
-	}
-
-	return result;
-}
-
-console.log( splitAndMerge2("Hello World!",":") );
-
+splitAndMerge("Hello World!", ',');
